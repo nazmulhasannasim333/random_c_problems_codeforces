50e2a2183c1a4108049a30510aa85b0c3386c49c
Nasim <mdnasimhosen333@gmail.com>
2025-05-06 23:16:14 +0600

    add implementation for C. Array Sum problem from Codeforces

---
+++ C_Array_Sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0;
+
+    while (scanf("%d", &num) != EOF)
+    {
+        sum += num;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
