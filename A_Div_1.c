e0856cea3cef173a41615962f1f9e6c0240c9acf
Nasim <mdnasimhosen333@gmail.com>
2025-05-10 23:48:29 +0600

    add implementation for A. Div1 problem from Codeforces

---
+++ A_Div_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    scanf("%d", &r);
+    if (r >= 1900)
+    {
+        printf("Can");
+    }
+    else
+    {
+        printf("Can't");
+    }
+    return 0;
+}
