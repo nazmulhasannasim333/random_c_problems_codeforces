f1670da1b4c0d8157da2882ce4c029848b797ea3
Nasim <mdnasimhosen333@gmail.com>
2025-05-16 21:01:16 +0600

    Add implementation for A. Single Number problem from Codeforces

---
+++ A_Single_Number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int result = 0;
+    int num;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        result ^= num;
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+}
