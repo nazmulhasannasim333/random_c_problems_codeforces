c056e2a406d9ae99417a82cd5bab51bd4928ce70
Nasim <mdnasimhosen333@gmail.com>
2025-05-17 23:08:24 +0600

    Add implementation for D. Special Summation problem from Codeforces

---
+++ D_Special_Summation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sum += a[i][j];
+            }
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
